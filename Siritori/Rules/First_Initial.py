d5e9e1616c2c7e8d6bbf1277dd1a536a995a70c7
dngmin <101644601+dngmin@users.noreply.github.com>
2023-04-15 19:36:28 +0900

    First_initial_rule + add_new_word_ko

---
+++ Siritori/Rules/First_Initial.py
@@ -0,0 +1,82 @@
+class First_initial():
+    def First_initial(lastinitial):
+        if lastinitial == '라':
+            lastinitial = '나'
+        elif lastinitial == '락':
+            lastinitial = '낙'
+        elif lastinitial == '란':
+            lastinitial = '난'
+        elif lastinitial == '랄':
+            lastinitial = '날'
+        elif lastinitial == '람':
+            lastinitial = '남'
+        elif lastinitial == '랍':
+            lastinitial = '남'
+        elif lastinitial == '랑':
+            lastinitial = '낭'
+        elif lastinitial == '래':
+            lastinitial = '내'
+        elif lastinitial == '랭':
+            lastinitial = '냉'
+        elif lastinitial == '냑' or lastinitial == '략':
+            lastinitial = '약'
+        elif lastinitial == '냥' or lastinitial == '량':
+            lastinitial = '양'
+        elif lastinitial == '녀' or lastinitial == '려':
+            lastinitial = '여'
+        elif lastinitial == '녁' or lastinitial == '력':
+            lastinitial = '역'
+        elif lastinitial == '년' or lastinitial == '련':
+            lastinitial = '연'
+        elif lastinitial == '녈' or lastinitial == '렬':
+            lastinitial = '열'
+        elif lastinitial == '념' or lastinitial == '렴':
+            lastinitial = '염'
+        elif lastinitial == '렵':
+            lastinitial = '엽'
+        elif lastinitial == '녕' or lastinitial == '령':
+            lastinitial = '영'
+        elif lastinitial == '녜' or lastinitial == '례':
+            lastinitial = '예'
+        elif lastinitial == '로':
+            lastinitial = '노'
+        elif lastinitial == '록':
+            lastinitial = '녹'
+        elif lastinitial == '론':
+            lastinitial = '논'
+        elif lastinitial == '롱':
+            lastinitial = '농'
+        elif lastinitial == '뢰':
+            lastinitial = '뇌'
+        elif lastinitial == '뇨' or lastinitial == '료':
+            lastinitial = '요'
+        elif lastinitial == '룡':
+            lastinitial = '용'
+        elif lastinitial == '루':
+            lastinitial = '누'
+        elif lastinitial == '뉴' or lastinitial == '류':
+            lastinitial = '유'
+        elif lastinitial == '뉵' or lastinitial == '륙':
+            lastinitial = '육'
+        elif lastinitial == '륜':
+            lastinitial = '윤'
+        elif lastinitial == '률':
+            lastinitial = '율'
+        elif lastinitial == '륭':
+            lastinitial = '융'
+        elif lastinitial == '륵':
+            lastinitial = '늑'
+        elif lastinitial == '름':
+            lastinitial = '늠'
+        elif lastinitial == '릉':
+            lastinitial = '능'
+        elif lastinitial == '니' or lastinitial == '리':
+            lastinitial = '이'
+        elif lastinitial == '린':
+            lastinitial = '인'
+        elif lastinitial == '림':
+            lastinitial = '임'
+        elif lastinitial == '립':
+            lastinitial = '입'
+            
+        return lastinitial
